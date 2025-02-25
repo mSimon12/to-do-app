ff0b55854960b123dc90e3b7dd1422c8478784ba
mSimon12 <marcelo_simon@hotmail.com>
2025-02-25 22:16:01 +0100

    feat: example of init and task create

--- api/src/to-do-api.go
+++ api/src/to-do-api.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"to-do-api/controllers"
+	"time"
+	"to-do-api/models"
 )
 
 func main() {
-	controllers.StartAPI()
+	//controllers.StartAPI()
+	// models.InitDatabase()
+	newTask := models.Task{Title: "task number 1", Description: "my description", Status: "backlog", Priority: 1, DueDate: time.Now().AddDate(0, 0, 7)}
+	models.CreateTask(newTask)
 }
