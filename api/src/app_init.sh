246f024cfb28d1e5233d39195dfceb2182c17686
mSimon12 <marcelo_simon@hotmail.com>
2025-02-26 19:41:25 +0100

    fix: stop local postgres in if it is running before run docker

--- api/src/app_init.sh
+++ api/src/app_init.sh
@@ -7,11 +7,14 @@ go get github.com/jackc/pgx/v5
 go get github.com/joho/godotenv
 
 ## PostgreSQL
-
 echo Starting To-Do App Database
+if sudo lsof -i :5432; then
+    echo Stopping local postgres
+    sudo service postgresql stop
+fi
 sudo docker compose -f models/database/docker-compose.yml up -d
 
-
+## Application
 echo Running To-Do App
 ## Run api in Go
 go run .
