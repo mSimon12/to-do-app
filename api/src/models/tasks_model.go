846f742498b6730f6c79a520623d89d83e03f538
mSimon12 <marcelo_simon@hotmail.com>
2025-02-26 19:42:05 +0100

    feat: modularize connection and add task update function

--- api/src/models/tasks_model.go
+++ api/src/models/tasks_model.go
@@ -20,19 +20,13 @@ type Task struct {
 }
 
 func CreateTask(newTask Task) {
-	createdAt := time.Now()
-
-	newTaskQuery := "INSERT INTO tasks (title, description, status, priority, created_at, due_date) VALUES ($1, $2, $3, $4, $5, $6);"
-
-	conn, err := pgx.Connect(context.Background(), getDatabaseUrl())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
+	conn := getDatabaseConnection()
 	defer conn.Close(context.Background())
 
 	// Add new task to DB
-	_, err = conn.Exec(context.Background(), newTaskQuery,
+	createdAt := time.Now()
+	newTaskQuery := "INSERT INTO tasks (title, description, status, priority, created_at, due_date) VALUES ($1, $2, $3, $4, $5, $6);"
+	_, err := conn.Exec(context.Background(), newTaskQuery,
 		newTask.Title,
 		newTask.Description,
 		newTask.Status,
@@ -48,15 +42,11 @@ func CreateTask(newTask Task) {
 }
 
 func QueryTask(taskId uint) {
-	conn, err := pgx.Connect(context.Background(), getDatabaseUrl())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
+	conn := getDatabaseConnection()
 	defer conn.Close(context.Background())
 
 	var queriedTask Task
-	err = conn.QueryRow(context.Background(), "SELECT * FROM tasks WHERE id=$1;", taskId).Scan(&queriedTask.Id, &queriedTask.Title,
+	err := conn.QueryRow(context.Background(), "SELECT * FROM tasks WHERE id=$1;", taskId).Scan(&queriedTask.Id, &queriedTask.Title,
 		&queriedTask.Description,
 		&queriedTask.Status,
 		&queriedTask.Priority,
@@ -69,19 +59,42 @@ func QueryTask(taskId uint) {
 	fmt.Println(queriedTask)
 }
 
-func DeleteTask(taskId uint) {
-	conn, err := pgx.Connect(context.Background(), getDatabaseUrl())
+func UpdateTask(updatedTask Task) {
+	conn := getDatabaseConnection()
+	defer conn.Close(context.Background())
+
+	// update task from DB
+	newTaskQuery := "UPDATE tasks SET title = $1, description= $2, status= $3, priority= $4, due_date= $5 WHERE id = $6;"
+	_, err := conn.Exec(context.Background(), newTaskQuery,
+		updatedTask.Title,
+		updatedTask.Description,
+		updatedTask.Status,
+		updatedTask.Priority,
+		updatedTask.DueDate,
+		updatedTask.Id)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
 	}
+}
+
+func DeleteTask(taskId uint) {
+	conn := getDatabaseConnection()
 	defer conn.Close(context.Background())
 
 	// Delete task from DB
-	_, err = conn.Exec(context.Background(), "DELETE FROM tasks WHERE id=$1;", taskId)
+	_, err := conn.Exec(context.Background(), "DELETE FROM tasks WHERE id=$1;", taskId)
 
 	if err != nil {
 		panic(err)
 	}
 
+}
+
+func getDatabaseConnection() *pgx.Conn {
+	conn, err := pgx.Connect(context.Background(), getDatabaseUrl())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	return conn
 }
