e5f07651f696d672cb316a645b907ce3a2f6a53b
mSimon12 <marcelo_simon@hotmail.com>
2025-03-07 15:14:12 +0100

    feat: task update and delete tests

--- api/src/models/models_test.go
+++ api/src/models/models_test.go
@@ -33,7 +33,7 @@ func TestAddTaskPattern(t *testing.T) {
 
 	expectedQuery := "INSERT INTO tasks \\(title, description, status, priority, created_at, due_date\\) VALUES \\(\\$1, \\$2, \\$3, \\$4, \\$5, \\$6\\) RETURNING id; "
 
-	// Expect an INSERT query with correct values
+	// Set SQL mock expectation
 	mockConn.ExpectQuery(expectedQuery).
 		WithArgs(newTask.Title, newTask.Description, newTask.Status, newTask.Priority, newTask.CreatedAt, newTask.DueDate).
 		WillReturnRows(pgxmock.NewRows([]string{"id"}).AddRow(uint(1)))
@@ -44,29 +44,105 @@ func TestAddTaskPattern(t *testing.T) {
 	// Assertions
 	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
 	assert.Equal(t, uint(1), taskID, "Returned value should be 1")
-
-	// Ensure all expected queries were executed
 	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
 }
 
-// func TestDeleteTaskPattern(t *testing.T) {
-// 	mockConn := setMockConnection()
-// 	defer mockConn.Close()
+func TestUpdateValidTask(t *testing.T) {
+	mockConn := setMockConnection()
+	defer mockConn.Close()
 
-// 	expectedQuery := "DELETE FROM tasks WHERE id=\\$1; "
+	// Define test task
+	updatedTask := Task{
+		Id:          1,
+		Title:       "Unit Test Task",
+		Description: "Mocked DB test",
+		Status:      "pending",
+		Priority:    1,
+		DueDate:     time.Now().Add(24 * time.Hour),
+	}
 
-// 	// Expect an INSERT query with correct values
-// 	mockConn.ExpectQuery(expectedQuery).
-// 		WithArgs(uint(1)).
-// 		WillReturnResult(psxmock.NewResult(1, 1))
+	expectedQuery := "UPDATE tasks SET title = \\$1, description= \\$2, status= \\$3, priority= \\$4, due_date= \\$5 WHERE id = \\$6;"
 
-// 	// Run function
-// 	taskID, err := AddTask(newTask)
+	// Set SQL mock expectation
+	mockConn.ExpectExec(expectedQuery).
+		WithArgs(updatedTask.Title, updatedTask.Description, updatedTask.Status, updatedTask.Priority, updatedTask.DueDate, updatedTask.Id).
+		WillReturnResult(pgxmock.NewResult("UPDATE", 1))
 
-// 	// Assertions
-// 	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
-// 	assert.Equal(t, uint(1), taskID, "Returned value should be 1")
+	// Run function
+	err := UpdateTask(updatedTask)
 
-// 	// Ensure all expected queries were executed
-// 	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
-// }
+	// Assertions
+	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
+	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
+}
+
+func TestUpdateInvalidTask(t *testing.T) {
+	mockConn := setMockConnection()
+	defer mockConn.Close()
+
+	// Define test task
+	updatedTask := Task{
+		Id:          1,
+		Title:       "Unit Test Task",
+		Description: "Mocked DB test",
+		Status:      "pending",
+		Priority:    1,
+		DueDate:     time.Now().Add(24 * time.Hour),
+	}
+
+	expectedQuery := "UPDATE tasks SET title = \\$1, description= \\$2, status= \\$3, priority= \\$4, due_date= \\$5 WHERE id = \\$6;"
+
+	// Set SQL mock expectation
+	mockConn.ExpectExec(expectedQuery).
+		WithArgs(updatedTask.Title, updatedTask.Description, updatedTask.Status, updatedTask.Priority, updatedTask.DueDate, updatedTask.Id).
+		WillReturnResult(pgxmock.NewResult("UPDATE", 0))
+
+	// Run function
+	err := UpdateTask(updatedTask)
+
+	// Assertions
+	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
+	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
+}
+
+func TestDeleteValidTask(t *testing.T) {
+	mockConn := setMockConnection()
+	defer mockConn.Close()
+
+	taskId := uint(1)
+
+	expectedQuery := "DELETE FROM tasks WHERE id=\\$1; "
+
+	// Set SQL mock expectation
+	mockConn.ExpectExec(expectedQuery).
+		WithArgs(taskId).
+		WillReturnResult(pgxmock.NewResult("DELETE", 1))
+
+	// Run function
+	err := DeleteTask(taskId)
+
+	// Assertions
+	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
+	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
+}
+
+func TestDeleteInvalidTask(t *testing.T) {
+	mockConn := setMockConnection()
+	defer mockConn.Close()
+
+	taskId := uint(1)
+
+	expectedQuery := "DELETE FROM tasks WHERE id=\\$1; "
+
+	// Set SQL mock expectation
+	mockConn.ExpectExec(expectedQuery).
+		WithArgs(taskId).
+		WillReturnResult(pgxmock.NewResult("DELETE", 0))
+
+	// Run function
+	err := DeleteTask(taskId)
+
+	// Assertions
+	assert.NoError(t, err, fmt.Sprintf("Unexpected error from function - err: %v", err))
+	assert.NoError(t, mockConn.ExpectationsWereMet(), "Expectations from SQL not met!")
+}
