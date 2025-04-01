a516219b53f59ea5b1110a22bd60308138f4d531
mSimon12 <marcelo_simon@hotmail.com>
2025-04-01 20:14:29 +0200

    feat: add swaggo documentation

--- api/src/to-do-api.go
+++ api/src/to-do-api.go
@@ -5,6 +5,10 @@ import (
 	"to-do-api/models"
 )
 
+// @title			To-Do API
+// @version		1.0
+// @description	To-Do List API for managing tasks to be done. It provides the basic functions of Creating, Updating, Deleting and Querying single or multiple tasks.
+// @termsOfService	http://swagger.io/terms/
 func main() {
 	models.InitDatabase()
 
